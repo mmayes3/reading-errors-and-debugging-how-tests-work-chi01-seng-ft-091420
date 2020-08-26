472bc841f954088da4f17da3e8e04bcd81fd5a43
classy-tier-0049 <mauricelynell@gmail.com>
2020-08-26 01:52:31 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,5 @@
 first_number = 12.00
 second_number = 4
-difference = first_number - second_number
 sum = first_number + second_number
+difference = first_number - second_number
 product = first_number*second_number
